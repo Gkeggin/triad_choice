aeeaf06a2e17129ae47b110c388774f738ca7c9c
Gkeggin <60437288+Gkeggin@users.noreply.github.com>
2025-07-01 10:18:19 +0100

    Update main.js
    Check to see if JS is working

--- triad_choice/main.js
+++ triad_choice/main.js
@@ -1,6 +1,2 @@
-window.onload = function() {
-  console.log("JS is loaded and running!");
-  const message = document.createElement("p");
-  message.textContent = "Hello from main.js! The script is running.";
-  document.body.appendChild(message);
-};
+document.body.insertAdjacentHTML('beforeend', '<p style="text-align:center; color: green;">JS is working!</p>');
+
