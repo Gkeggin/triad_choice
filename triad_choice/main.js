2aec56d526342c90b9e7b8494f451689cfc81159
Gkeggin <60437288+Gkeggin@users.noreply.github.com>
2025-07-01 10:13:44 +0100

    Update main.js
    simple JS code to make sure it's working

--- triad_choice/main.js
+++ triad_choice/main.js
@@ -1,10 +1,5 @@
-// main.js
-
-// This will run when the page is loaded
 window.onload = function() {
   console.log("JS is loaded and running!");
-
-  // Just add some text to the body so you can see it's working
   const message = document.createElement("p");
   message.textContent = "Hello from main.js! The script is running.";
   document.body.appendChild(message);
